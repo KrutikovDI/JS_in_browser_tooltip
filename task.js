68614bff608bdd38fe0672f98e93c3a3c28f0c7e
Danil Krutikov <kdanil.i@yandex.ru>
2023-12-18 19:43:51 +0700

    Second commit

--- task.js
+++ task.js
@@ -1,13 +1,19 @@
-const a = document.getElementsByClassName("has-tooltip")
-for (i=0; i<a.length; i++) {
-  a[i].insertAdjacentHTML("afterEnd", `<div class='tooltip'>${a[i].title}</div>`)}
-for (i=0; i<a.length; i++) {
+const a = document.getElementsByClassName("has-tooltip");
+for (let i = 0; i < a.length; i++) {
+  a[i].insertAdjacentHTML("afterEnd", `<div class='tooltip'>${a[i].title}</div>`);
+}
+for (let i = 0; i < a.length; i++) {
   a[i].addEventListener("click", (e) => {
-    e.preventDefault()
-    const tooltip = document.getElementsByClassName("tooltip")
-    for (j=0; j<tooltip.length; j++) {
-      tooltip[j].className = "tooltip"
+    e.preventDefault();
+    const tooltip = document.getElementsByClassName("tooltip");
+    for (let j = 0; j < tooltip.length; j++) {
+      if (tooltip[j] != e.target.nextSibling) {
+        tooltip[j].classList.remove("tooltip_active");
+      }
     }
-    e.target.nextSibling.classList.add("tooltip_active")
-  })
+    const domRect = e.target.getBoundingClientRect();
+    e.target.nextSibling.style.position = "absolute";
+    e.target.nextSibling.style.left = `${domRect.left}px`;
+    e.target.nextSibling.classList.toggle("tooltip_active");
+  });
 }
